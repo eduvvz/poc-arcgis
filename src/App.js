24876afe67b8f914a0d6a778ac942afed016acbc
Eduardo Cerqueira e Silva <eduardo.cerqueira@MacBook-Pro-de-MACBOOKDOC-2.local>
2022-03-03 17:48:03 -0300

    add cruise, and yout path

--- src/App.js
+++ src/App.js
@@ -21,12 +21,33 @@ const satellites_mock = [
   { name: 'sat 5', x: -80.030353, y: 38.868616, z: 500000 }
 ];
 
+const cruises_mock = [
+  {
+    name: 'cruise 1',
+    x: -92.600786,
+    y: 28.598917,
+    z: 50000,
+    paths: [
+      [-92.600786, 28.598917],
+      [-84.764734, 24.253481],
+      [-79.690918, 23.885950],
+      [-80.134593, 25.616391]
+    ],
+  },
+];
+
 function App() {
   const [sceneView, setSceneView] = useState();
   const [activeSat, setActiveSat] = useState();
+  const [activeCruise, setActiveCruise] = useState();
 
   const customContentRender = (sat) => {
     setActiveSat(sat);
+    return <div id="popup-render-custom" />
+  }
+
+  const customContentRenderCruizer = (cru) => {
+    setActiveCruise(cru);
     return <div id="popup-render-custom" />
   }
 
@@ -46,11 +67,37 @@ function App() {
       viewingMode: 'global',
     });
 
-    view.on('click', (ev) => {
+    view.on('click', () => {
       setActiveSat(undefined);
+      setActiveCruise(undefined);
     })
 
     const satelliteLayer = new GraphicsLayer();
+    const cruiseLayer = new GraphicsLayer();
+
+    cruises_mock.forEach(cru => {
+      const template = {
+        content: () => customContentRenderCruizer(cru),
+      }
+
+      let graphic = new Graphic({
+        geometry: {
+          type: "point",
+          x: cru.x,
+          y: cru.y,
+          z: cru.z
+        },
+        symbol: {
+          type: "picture-marker",
+          url: require("./cruise-ship.png"),
+          width: "30px",
+          height: "10px",
+        },
+        popupTemplate: template
+      });
+
+      cruiseLayer.add(graphic);
+    });
 
     satellites_mock.forEach(sat => {
 
@@ -68,8 +115,8 @@ function App() {
         symbol: {
           type: "picture-marker",  // autocasts as new PictureMarkerSymbol()
           url: require("./satellite.png"),
-          width: "50px",
-          height: "30px",
+          width: "40px",
+          height: "20px",
         },
         popupTemplate: template
       });
@@ -78,16 +125,51 @@ function App() {
 
     });
 
-    map.add(satelliteLayer);
+    map.addMany([cruiseLayer, satelliteLayer]);
 
     setSceneView(view);
   }, [])
 
-  const moveCamera = useCallback(() => {
-    function shiftCamera(sat) {
+  const showCruisePath = useCallback(() => {
+    const polylineGraphic = new Graphic({
+      geometry: {
+        type: "polyline",
+        paths: activeCruise.paths,
+      },
+      symbol: {
+        type: "simple-line",
+        color: "white",
+        width: "2px",
+        style: "long-dash",
+        join: "bevel",
+      },
+    });
+
+    const lastPath = activeCruise.paths.slice(-1).pop();
+
+    const markGraphic = new Graphic({
+      geometry: {
+        type: "point",
+        x: lastPath[0],
+        y: lastPath[1],
+        z: 50000
+      },
+      symbol: {
+        type: "picture-marker",
+        url: require("./pin.png"),
+        width: "30px",
+        height: "30px",
+      },
+    });
+
+    sceneView.graphics.addMany([polylineGraphic, markGraphic]);
+  }, [sceneView, activeCruise])
+
+  const moveCamera = useCallback((x, y) => {
+    function shiftCamera() {
       const camera = sceneView.camera.clone();
-      camera.position.longitude = sat.x;
-      camera.position.latitude = sat.y;
+      camera.position.longitude = x;
+      camera.position.latitude = y;
       return camera;
     }
 
@@ -99,19 +181,30 @@ function App() {
 
     sceneView
       .goTo(
-        shiftCamera(activeSat),
+        shiftCamera(),
         {
           speedFactor: 1,
           easing: "linear"
         }
       )
       .catch(catchAbortError);
-  }, [activeSat, sceneView])
+  }, [sceneView])
+
+  useEffect(() => {
+    if (!sceneView || !activeCruise) {
+      console.log(sceneView?.graphics)
+      sceneView?.graphics.removeAll();
+      return;
+    };
+
+    moveCamera(activeCruise.x, activeCruise.y);
+    showCruisePath();
+  }, [activeCruise, moveCamera, sceneView, showCruisePath])
 
   useEffect(() => {
     if (!sceneView || !activeSat) return;
 
-    moveCamera();
+    moveCamera(activeSat.x, activeSat.y);
   }, [activeSat, moveCamera, sceneView])
 
   return (
